283c9410855bea9fa0ae3f8d1fbf8363f0e01b82
unknown <madhumitha>
2026-08-12 09:43:52 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,73 @@
+#Task 1
+name='Madhumitha'
+age=22
+city='Nizamabad'
+print("Name : " , name)
+print("Age : " , age)
+print("City : " , city)
+
+#Task 2
+std_name='Madhumitha'
+std_age=22
+std_marks=95
+#1std_marks=86#invalid because identifier can't start with number
+
+print("student name :" , std_name)
+print("student age : " , std_age)
+print("student marks : " , std_marks)
+
+#Task 3
+Name='Madhumitha' #stores my name that is madhumitha in Name
+Age=22 #stores my age that is 22 in Age
+print("Name :" , Name)
+print("Age : " , Age)
+
+#Task 4
+'''
+this program will print the three messages about the python directly through print statement
+'''
+print("Welcome to python")
+print("I am learning programming")
+print("Python is easy to learn")
+
+#Task 5
+name='Madhumitha'
+age=22
+Height=5.3
+student='True'
+print("Name : ",name)
+print("Age : ", age)
+print("Height : ", Height)
+print("is_student : ",student)
+
+#Task 6
+name,age,city='madhu',22,'nizamabad'
+print("name :",name)
+print("Age :",age)
+print("city :",city)
+
+#Task 7
+Age=22
+print("Age :",Age)
+Age=23
+print("age :",Age)
+
+#Task 8
+a=10
+b=20
+print("a: ",a)
+print("b :",b)
+a,b=b,a
+print("a :",a)
+print("b :",b)
+
+#Task 9
+name='Madhumitha'
+print("Name :",name)
+# del name
+# print("Name :",name) #showing error like name is not defined
+
+#Task 10
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))

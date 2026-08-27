60c98e54c19d2c8f38e28a461f79335102c65c80
unknown <madhumitha>
2026-08-27 19:46:28 +0530

    practice conditional_statements

--- day08.py
+++ day08.py
@@ -1,5 +1,5 @@
 #https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
-'''if __name__ == '__main__':
+if __name__ == '__main__':
     n = int(input().strip())
     if n%2!=0:
         print('Weird')
@@ -9,11 +9,11 @@
         elif 6<=n<=20:
             print('Weird')
         else:
-            print('Not Weird')'''
+            print('Not Weird')
 
 
 #https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
-'''def is_leap(year):
+def is_leap(year):
     
     if year%400==0:
         return True
@@ -23,7 +23,8 @@
         return True
     else:
         return False
-year = int(input())'''
+year = int(input())
+
 
 #match statement 
 n=int(input("Enter a number : "))
